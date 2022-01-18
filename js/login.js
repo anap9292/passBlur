998d4a2f39145a3a9fb0aff040de9f251922fdc2
Anahi <anahiandrea.p@gmail.com>
2022-01-18 18:05:31 -0300

    Trabajo practico terminado

---
+++ js/login.js
@@ -0,0 +1,11 @@
+let password= document.querySelector('#password')
+ let background=document.querySelector('#background') 
+  password.addEventListener('input',(e)=>{    
+       let entrada=e.target.value      
+       let cantidad=entrada.length     
+       let valor_del_blur= 20 - (cantidad*5)    
+       //console.log(cantidad)
+       //console.log(entrada)
+
+  background.style.filter=`blur(${valor_del_blur}px)`
+})
